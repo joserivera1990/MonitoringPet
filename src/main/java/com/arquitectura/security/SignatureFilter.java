b9806c958e136ae9a1c9988023b5f46997c4222e
joserivera1990 <ing.joserivera1990@gmail.com>
2016-04-25 19:47:07 -0500

    Ajustes firma

--- src/main/java/com/arquitectura/security/SignatureFilter.java
+++ src/main/java/com/arquitectura/security/SignatureFilter.java
@@ -8,7 +8,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.MessageDigest;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -19,81 +18,77 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
- 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SignatureFilter implements Filter {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(FilteredRequest.class);
 	
 	class FilteredRequest extends HttpServletRequestWrapper {
  
+		
 		private String signature;
-		private String _body;
+		private String body;
 
 		
     	/* These are the characters allowed by the Javascript validation */
     	public FilteredRequest(ServletRequest request) throws IOException {
     		super((HttpServletRequest)request);
-            _body = "";
+    		body = "";
             try (BufferedReader bufferedReader = request.getReader())
             {
                 String line;
                 while ((line = bufferedReader.readLine()) != null)
-                    _body += line;
+                	body += line;
                 
-                String trama = _body.substring(2, _body.indexOf("signature") - 1);
-                signature = _body.substring(_body.indexOf("signature") + 13, _body.length() - 2);
-                System.out.println(signature + " ---- signature " + trama);
-                
+                String trama = body.substring(2, body.indexOf("signature") - 1);
+                signature = body.substring(body.indexOf("signature") + 13, body.length() - 2);                
                 MessageDigest md = MessageDigest.getInstance("SHA-256");
     			md.update(trama.getBytes());
 
-    			byte byteData[] = md.digest();
+    			byte[] byteData = md.digest();
 
     			//convert the byte to hex format method 1
-    			StringBuffer sb = new StringBuffer();
+    			StringBuilder sb = new StringBuilder();
     			for (int i = 0; i < byteData.length; i++) {
     				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
     			}
     			
     			System.out.println(signature + " --- " + sb.toString());
     			if (signature.equals(sb.toString())) {
-    				_body = _body.substring(0, _body.length() - 1);
-    				_body += ", \"isValid\":" + "\"true\" }";
+    				body = body.substring(0, body.length() - 1);
+    				body += ", \"isValid\":" + "\"true\" }";
     			} else {
-    				_body = _body.substring(0, _body.length() - 1);
-    				_body += ", \"isValid\":" + "\"false\" }";
+    				body = body.substring(0, body.length() - 1);
+    				body += ", \"isValid\":" + "\"false\" }";
     			}
-                
-                System.out.println(_body + " body");
             } catch (Exception e) {
+            	 LOGGER.error("Error filter [{}]", e);
 			}
     	}
  
     	@Override
         public ServletInputStream getInputStream() throws IOException
         {
-            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(_body.getBytes());
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
             return new ServletInputStream()
-            {
-                public int read() throws IOException
-                {
+            {   
+            	@Override
+                public int read() throws IOException{
                     return byteArrayInputStream.read();
                 }
-
 				@Override
 				public boolean isFinished() {
-					// TODO Auto-generated method stub
 					return false;
 				}
-
 				@Override
 				public boolean isReady() {
-					// TODO Auto-generated method stub
 					return false;
 				}
-
 				@Override
 				public void setReadListener(ReadListener listener) {
-					// TODO Auto-generated method stub
-					
+					// Do nothing because
 				}
             };
         }
@@ -104,16 +99,21 @@ public class SignatureFilter implements Filter {
             return new BufferedReader(new InputStreamReader(this.getInputStream()));
         }
     }
- 
+    
+	@Override
 	public void init(FilterConfig config) throws ServletException {
+		// Do nothing because
 	}
- 
+    
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws java.io.IOException, ServletException {
-		chain.doFilter(new FilteredRequest(request), response);
+		              chain.doFilter(new FilteredRequest(request), response);
 	}
- 
+    
+	@Override
 	public void destroy() {
+		// Do nothing because
 	}
  
 }
